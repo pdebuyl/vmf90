e2c88f5e14cd2d1ed61e48edb8002dc47e1cd4a7
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2011-08-22 23:04:01 -0400

    Added copyright information.

--- src/spline_module.f90
+++ src/spline_module.f90
@@ -1,3 +1,20 @@
+! Copyright (C) 2009-2011 Pierre de Buyl
+
+! This file is part of vmf90
+
+! vmf90 is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+
+! vmf90 is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! You should have received a copy of the GNU General Public License
+! along with vmf90.  If not, see <http://www.gnu.org/licenses/>.
+
 !> Module for the computation of natural and periodic spline interpolation.
 !!
 !! The natural spline has zero-valued second derivatives at the boundaries. The periodic splines
